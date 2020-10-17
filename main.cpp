b2f886aaa98727bc13e690e5c44a89cc74891ce4
YeEeck <yechenkai@foxmail.com>
2020-10-18 03:06:53 +0800

    del

--- main.cpp
+++ main.cpp
@@ -6,23 +6,6 @@
 
 using namespace std;
 
-void prtline() {
-    int delay_s;
-    ifstream infile;
-    default_random_engine e(time(nullptr));
-    uniform_int_distribution<unsigned> ran(0, 52);
-    infile.open("code.txt", ios::in);
-    string line;
-    system("color 02");
-    while (getline(infile, line)) {
-        delay_s = ran(e);
-        if (delay_s >= 50) {
-            delay_s = 250;
-        }
-        Sleep(delay_s);
-        cout << line << endl;
-    }
-}
 
 void prtchar() {
     int delay_s;
